c7745b9e53dfbd5194e76651d272627713c0856b
uuuuuuuk <s22034@gsm.hs.kr>
2023-03-02 18:28:21 +0900

    test

---
+++ src/test/java/mpersand/Gmuwiki/GMuwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package mpersand.Gmuwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GMuwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
