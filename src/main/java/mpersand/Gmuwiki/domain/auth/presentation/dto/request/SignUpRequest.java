75c0d1a1642c6e1396a6fbc36601085152fdfc98
uuuuuuuk <s22034@gsm.hs.kr>
2023-03-07 22:52:49 +0900

    :memo: SignUpRequest 추가

---
+++ src/main/java/mpersand/Gmuwiki/domain/auth/presentation/dto/request/SignUpRequest.java
@@ -0,0 +1,31 @@
+package mpersand.Gmuwiki.domain.auth.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mpersand.Gmuwiki.domain.user.enums.Role;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpRequest {
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 특수문자가 1개 이상 포함되어야 하고 글자 수는 8자 이상, 16자 이내로 입력해야 합니다.")
+    private String password;
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    private String name;
+
+    @NotBlank(message = "학번은 필수 입력 값입니다.")
+    private String number;
+
+    @NotBlank(message = "역할은 필수 입력 값입니다.")
+    private Role role;
+}
