ffbb36fe506c6423383de1fe0276526d469e790d
uuuuuuuk <s22034@gsm.hs.kr>
2023-03-09 08:27:20 +0900

    :memo: 비밀번호 체크 유틸 추가

--- src/main/java/mpersand/Gmuwiki/global/util/UserUtil.java
+++ src/main/java/mpersand/Gmuwiki/global/util/UserUtil.java
@@ -1,10 +1,12 @@
 package mpersand.Gmuwiki.global.util;
 
 import lombok.RequiredArgsConstructor;
+import mpersand.Gmuwiki.domain.auth.exception.MisMatchPasswordException;
 import mpersand.Gmuwiki.domain.auth.exception.UserNotFoundException;
 import mpersand.Gmuwiki.domain.user.entity.User;
 import mpersand.Gmuwiki.domain.user.repository.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,10 +14,22 @@ import org.springframework.stereotype.Component;
 public class UserUtil {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User currentUser() {
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UserNotFoundException("존재하지 않는 사용자입니다"));
+    }
+
+    public void checkPassword(User user, String password) {
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new MisMatchPasswordException("비밀번호가 일치하지 않습니다.");
+        }
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
     }
 }
