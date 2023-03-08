6027ff92a82df9c888bc17661be521caea9bfa87
uuuuuuuk <s22034@gsm.hs.kr>
2023-03-09 08:29:53 +0900

    :memo: LoginRequest 추가

---
+++ src/main/java/mpersand/Gmuwiki/domain/auth/presentation/dto/request/LoginRequest.java
@@ -0,0 +1,19 @@
+package mpersand.Gmuwiki.domain.auth.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    private String password;
+}
