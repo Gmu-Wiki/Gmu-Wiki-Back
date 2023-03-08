4108d2e8bd580d4d93aaf78568ea31c02844600a
uuuuuuuk <s22034@gsm.hs.kr>
2023-03-09 08:30:06 +0900

    :memo: LoginResponse 추가

---
+++ src/main/java/mpersand/Gmuwiki/domain/auth/presentation/dto/response/LoginResponse.java
@@ -0,0 +1,23 @@
+package mpersand.Gmuwiki.domain.auth.presentation.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginResponse {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private ZonedDateTime expiredAt;
+}
