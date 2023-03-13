98de34281225e7629e0afe0dc944e35677995a69
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-13 17:20:52 +0900

    entity작성

--- src/main/java/mpersand/Gmuwiki/domain/notice/Notice.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/Notice.java
@@ -1,4 +1,39 @@
 package mpersand.Gmuwiki.domain.notice;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mpersand.Gmuwiki.domain.notice.presentation.dto.reqeust.ModifyNoticeRequest;
+import mpersand.Gmuwiki.domain.user.entity.User;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Notice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "email")
+    private User user;
+
+    public void modifyPost(ModifyNoticeRequest modifyNoticeRequest) {
+        this.title = modifyNoticeRequest.getTitle();
+        this.content = modifyNoticeRequest.getContent();
+    }
 }
