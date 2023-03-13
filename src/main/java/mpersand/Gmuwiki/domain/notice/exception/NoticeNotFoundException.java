66588961aefb3db978fefd0db4df6e2a9239b1b5
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-13 17:15:19 +0900

    add NoticeNotFoundException ErrorCode

--- src/main/java/mpersand/Gmuwiki/domain/notice/exception/NoticeNotFoundException.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/exception/NoticeNotFoundException.java
@@ -1,4 +1,12 @@
 package mpersand.Gmuwiki.domain.notice.exception;
 
-public class NoticeNotFoundException {
+import mpersand.Gmuwiki.global.error.ErrorCode;
+
+public class NoticeNotFoundException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public NoticeNotFoundException(String message){
+        super(message);
+        this.errorCode = ErrorCode.POST_NOT_FOUND;
+    }
 }
