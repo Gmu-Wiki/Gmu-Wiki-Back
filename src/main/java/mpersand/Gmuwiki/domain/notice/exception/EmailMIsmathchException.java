5b1437abb6255cff67aa7d538ae3ba92699094c6
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-13 17:12:21 +0900

    add ErrorCodeMesaage

--- src/main/java/mpersand/Gmuwiki/domain/notice/exception/EmailMIsmathchException.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/exception/EmailMIsmathchException.java
@@ -1,4 +1,14 @@
 package mpersand.Gmuwiki.domain.notice.exception;
 
-public class EmailMIsmathchException {
+import lombok.Getter;
+import mpersand.Gmuwiki.global.error.ErrorCode;
+
+@Getter
+public class EmailMIsmathchException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public EmailMIsmathchException(String message){
+        super(message);
+        this.errorCode = ErrorCode.EMAIL_MISMATCH;
+    }
 }
