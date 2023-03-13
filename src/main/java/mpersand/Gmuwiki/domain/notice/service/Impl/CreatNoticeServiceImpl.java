c29ed39805d39b9e5866d082b25c37ba44455a8d
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-13 16:09:04 +0900

    add service

--- src/main/java/mpersand/Gmuwiki/domain/notice/service/Impl/CreatNoticeServiceImpl.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/service/Impl/CreatNoticeServiceImpl.java
@@ -1,13 +1,8 @@
 package mpersand.Gmuwiki.domain.notice.service.Impl;
 
-import lombok.RequiredArgsConstructor;
-import mpersand.Gmuwiki.domain.notice.repository.NoticeRespository;
-import mpersand.Gmuwiki.domain.notice.service.CreateNoticeService;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
+import mpersand.Gmuwiki.domain.notice.service.CreateNoticeService;
+
 public class CreatNoticeServiceImpl implements CreateNoticeService {
 
 
