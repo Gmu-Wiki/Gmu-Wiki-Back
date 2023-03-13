a2b71c03fba620e0fd9407ba47a107eaf6ccd593
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-13 17:33:54 +0900

    adminController 작성

--- src/main/java/mpersand/Gmuwiki/domain/notice/presentation/AdminNoticeController.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/AdminNoticeController.java
@@ -1,4 +1,41 @@
 package mpersand.Gmuwiki.domain.notice.presentation;
 
+import lombok.RequiredArgsConstructor;
+import mpersand.Gmuwiki.domain.notice.presentation.dto.reqeust.CreateNoticeRequest;
+import mpersand.Gmuwiki.domain.notice.presentation.dto.reqeust.ModifyNoticeRequest;
+import mpersand.Gmuwiki.domain.notice.service.CreateNoticeService;
+import mpersand.Gmuwiki.domain.notice.service.ModifyNoticeService;
+import mpersand.Gmuwiki.domain.notice.service.RemoveNoticeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/notice")
+@RequiredArgsConstructor
 public class AdminNoticeController {
+
+    private final CreateNoticeService createNoticeService;
+    private final ModifyNoticeService modifyNoticeService;
+    private final RemoveNoticeService removeNoticeService;
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid CreateNoticeRequest createNoticeRequest){
+        createNoticeService.create(createNoticeRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> modify(@PathVariable Long id, @RequestBody @Valid ModifyNoticeRequest modifyNoticeRequest){
+        modifyNoticeService.modify(id, modifyNoticeRequest);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        removeNoticeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
