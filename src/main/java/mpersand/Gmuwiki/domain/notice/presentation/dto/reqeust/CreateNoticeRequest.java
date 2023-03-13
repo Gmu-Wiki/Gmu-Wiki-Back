1e3e492be951be5b0c44666f42a3b1421351e400
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-13 17:23:55 +0900

    request dto 작성

--- src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/reqeust/CreateNoticeRequest.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/reqeust/CreateNoticeRequest.java
@@ -1,4 +1,19 @@
 package mpersand.Gmuwiki.domain.notice.presentation.dto.reqeust;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
 public class CreateNoticeRequest {
+    @NotBlank(message = "제목은 필수 입력값입니다")
+    private final String title;
+    @NotBlank(message = "내용은 필수 입력값입니다")
+    private final String content;
+    @NotBlank(message = "이름은 필수입력값입니다")
+    private final String name;
 }
