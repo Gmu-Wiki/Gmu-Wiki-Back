c6d4fea714c57e6fe64feba0f2eefb49d4d737a1
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-14 19:39:05 +0900

    edit Service

--- src/main/java/mpersand/Gmuwiki/domain/notice/service/ModifyNoticeService.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/service/ModifyNoticeService.java
@@ -3,5 +3,5 @@ package mpersand.Gmuwiki.domain.notice.service;
 import mpersand.Gmuwiki.domain.notice.presentation.dto.reqeust.ModifyNoticeRequest;
 
 public interface ModifyNoticeService {
-    public void modify(Long id, ModifyNoticeRequest modifyNoticeRequest);
+    void modify(Long id, ModifyNoticeRequest modifyNoticeRequest);
 }
