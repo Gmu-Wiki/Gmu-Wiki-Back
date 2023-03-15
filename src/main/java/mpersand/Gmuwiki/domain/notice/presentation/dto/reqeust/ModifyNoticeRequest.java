06c49f1cd5c9a5978628483e921402a5a1321ce9
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-15 20:21:06 +0900

    :memo: 가독성을 위해 줄 간격 추가

--- src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/reqeust/ModifyNoticeRequest.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/reqeust/ModifyNoticeRequest.java
@@ -10,8 +10,10 @@ import javax.validation.constraints.NotBlank;
 public class ModifyNoticeRequest {
     @NotBlank(message = "제목은 필수 입력값입니다")
     private String title;
+
     @NotBlank(message = "내용은 필수 입력값입니다")
     private String content;
+
     @NotBlank(message = "이름은 필수입력값입니다")
     private String name;
 }
