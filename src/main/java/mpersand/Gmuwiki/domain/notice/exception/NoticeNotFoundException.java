dd61d3e5add1ec36d7706088ba8827753f556759
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-18 14:33:47 +0900

    :memo: @Getter 추가

--- src/main/java/mpersand/Gmuwiki/domain/notice/exception/NoticeNotFoundException.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/exception/NoticeNotFoundException.java
@@ -1,7 +1,9 @@
 package mpersand.Gmuwiki.domain.notice.exception;
 
+import lombok.Getter;
 import mpersand.Gmuwiki.global.error.ErrorCode;
 
+@Getter
 public class NoticeNotFoundException extends RuntimeException{
     private final ErrorCode errorCode;
 
