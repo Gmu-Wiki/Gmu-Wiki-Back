e9390736ce0e6af8ccbc3e523c9564faed8ec1be
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-18 11:56:57 +0900

    :memo: /member/notice/** 추가

--- src/main/java/mpersand/Gmuwiki/global/config/security/SecurityConfig.java
+++ src/main/java/mpersand/Gmuwiki/global/config/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/admin/notice/**").permitAll()
+                .antMatchers("/member/notice/**").permitAll()
 
                 .anyRequest().authenticated();
 
