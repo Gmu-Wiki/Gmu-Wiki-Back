b3d511c6a4000cd28db4fdd24fdcadee9ff55aa5
uuuuuuuk <s22034@gsm.hs.kr>
2023-03-18 16:10:44 +0900

    :memo: EmailController 에 이메일 인증번호 확인 메서드 등록

--- src/main/java/mpersand/Gmuwiki/domain/email/presentation/EmailController.java
+++ src/main/java/mpersand/Gmuwiki/domain/email/presentation/EmailController.java
@@ -2,15 +2,14 @@ package mpersand.Gmuwiki.domain.email.presentation;
 
 import lombok.RequiredArgsConstructor;
 import mpersand.Gmuwiki.domain.email.presentation.dto.request.EmailSendRequest;
+import mpersand.Gmuwiki.domain.email.service.EmailCheckService;
 import mpersand.Gmuwiki.domain.email.service.EmailSendService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,10 +17,17 @@ import javax.validation.Valid;
 public class EmailController {
 
     private final EmailSendService emailSendService;
+    private final EmailCheckService emailCheckService;
 
     @PostMapping
     public ResponseEntity<Void> sendEmail(@RequestBody @Valid EmailSendRequest emailSendRequest) {
         emailSendService.execute(emailSendRequest);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @RequestMapping(method = RequestMethod.HEAD)
+    public ResponseEntity<Void> AuthCheck(@Email @RequestParam String email, @RequestParam String authCode) {
+        emailCheckService.execute(email, authCode);
+        return ResponseEntity.ok().build();
     }
 }
