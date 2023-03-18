6ac8f84e568830df054aa9c2a6e8183032292a54
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-18 13:47:13 +0900

    :memo: 사용하지 않는 import문 삭제

--- src/main/java/mpersand/Gmuwiki/domain/notice/repository/NoticeRepository.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/repository/NoticeRepository.java
@@ -1,11 +1,8 @@
 package mpersand.Gmuwiki.domain.notice.repository;
 
 import mpersand.Gmuwiki.domain.notice.entity.Notice;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 }
