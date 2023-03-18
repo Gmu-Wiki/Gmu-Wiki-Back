bfa52d9e3ff3d858d39333c9ccfa19464e16ae65
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-18 11:23:51 +0900

    :sparkles: :memo:NoticeResponse 생성

---
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/response/NoticeResponse.java
@@ -0,0 +1,15 @@
+package mpersand.Gmuwiki.domain.notice.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NoticeResponse {
+
+    private Long id;
+    private String title;
+    private String name;
+}
