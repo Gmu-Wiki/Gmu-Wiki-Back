f9d654b2e724e05849e8ca9050400b9b378d2a0a
uuuuuuuk <s22034@gsm.hs.kr>
2023-03-23 23:53:30 +0900

    :sparkles: UserController 생성 및 비밀번호 변경 서비스 등록

---
+++ src/main/java/mpersand/Gmuwiki/domain/user/presentation/UserController.java
@@ -0,0 +1,26 @@
+package mpersand.Gmuwiki.domain.user.presentation;
+
+import lombok.RequiredArgsConstructor;
+import mpersand.Gmuwiki.domain.user.presentation.dto.request.ChangePasswordRequest;
+import mpersand.Gmuwiki.domain.user.service.ChangePasswordService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final ChangePasswordService changePasswordService;
+
+    @PatchMapping("/change-pw")
+    public ResponseEntity<Void> ChangePassword(@RequestBody @Valid ChangePasswordRequest changePasswordRequest) {
+        changePasswordService.execute(changePasswordRequest);
+        return ResponseEntity.noContent().build();
+    }
+}
