440396a7a57f396bfbc3467072a5c329eb5299a5
정찬교 <jeongchangyo71@gmail.com>
2023-03-30 09:19:22 +0900

    안쓰는 import문 삭제

--- src/main/java/mpersand/Gmuwiki/domain/notice/presentation/AdminNoticeController.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/AdminNoticeController.java
@@ -1,14 +1,12 @@
 package mpersand.Gmuwiki.domain.notice.presentation;
 
 import lombok.RequiredArgsConstructor;
-import mpersand.Gmuwiki.domain.notice.presentation.dto.request.CreateNoticeRequest;
 import mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeResponse;
 import mpersand.Gmuwiki.domain.notice.service.ListNoticeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
