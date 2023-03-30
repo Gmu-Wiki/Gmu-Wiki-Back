2e016380faf7fc1baa8e74b0aab25edf47cd3c1e
정찬교 <jeongchangyo71@gmail.com>
2023-03-30 09:25:34 +0900

    :sparkles: NoticeResponse dto작성

---
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/response/NoticeResponse.java
@@ -0,0 +1,15 @@
+package mpersand.Gmuwiki.domain.notice.presentation.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class NoticeResponse {
+    private Long id;
+    private String title;
+    private String content;
+}
