63e75da0acf34fbf624376b36a400b864502a123
정찬교 <jeongchangyo71@gmail.com>
2023-03-30 19:31:01 +0900

    :sparkles: MemberNoticeController 공지사항 상세보기 코드 추가

--- src/main/java/mpersand/Gmuwiki/domain/notice/presentation/MemberNoticeController.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/MemberNoticeController.java
@@ -1,8 +1,10 @@
 package mpersand.Gmuwiki.domain.notice.presentation;
 
 import lombok.RequiredArgsConstructor;
+import mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeIdResponse;
 import mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeResponse;
 import mpersand.Gmuwiki.domain.notice.service.ListNoticeService;
+import mpersand.Gmuwiki.domain.notice.service.OneNoticeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +17,17 @@ import java.util.List;
 public class MemberNoticeController {
 
     private final ListNoticeService listNoticeService;
+    private final OneNoticeService oneNoticeService;
 
     @GetMapping("/list")
     public ResponseEntity<List<NoticeResponse>> findAll(){
         List<NoticeResponse> list = listNoticeService.execute();
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<NoticeIdResponse> findOne(@PathVariable("id") Long id){
+        NoticeIdResponse oneFindById = oneNoticeService.execute(id);
+        return new ResponseEntity<>(oneFindById,HttpStatus.OK);
     }
 }
