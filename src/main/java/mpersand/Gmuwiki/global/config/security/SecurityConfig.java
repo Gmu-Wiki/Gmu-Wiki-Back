d27ece3bdc9cdc9015016a6e91b8e65ac8344538
jeong <jeongchangyo71@gmail.com>
2023-04-03 20:46:24 +0900

    :memo: .hasAuthority로 권한부여 성공

--- src/main/java/mpersand/Gmuwiki/global/config/security/SecurityConfig.java
+++ src/main/java/mpersand/Gmuwiki/global/config/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package mpersand.Gmuwiki.global.config.security;
 
 import lombok.RequiredArgsConstructor;
+import mpersand.Gmuwiki.domain.user.enums.Role;
 import mpersand.Gmuwiki.global.filter.JwtRequestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,8 +37,8 @@ public class SecurityConfig {
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/email/**").permitAll()
-                .antMatchers("/admin/notice/**").hasRole("ADMIN")
-                .antMatchers("/user/notice/**").hasRole("USER")
+                .antMatchers("/admin/notice/**").hasAuthority("ADMIN")
+                .antMatchers("/user/notice/**").hasAuthority("USER")
                 .anyRequest().authenticated();
 
         http
