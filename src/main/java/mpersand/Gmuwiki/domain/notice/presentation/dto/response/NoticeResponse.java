a4282f31532d681b31ce20e08ad2067b2481237b
jeong <jeongchangyo71@gmail.com>
2023-04-04 11:04:58 +0900

    :memo: @Builder를 사용하고 NoticeResposne값을 바꿈

--- src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/response/NoticeResponse.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/response/NoticeResponse.java
@@ -1,15 +1,12 @@
 package mpersand.Gmuwiki.domain.notice.presentation.dto.response;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class NoticeResponse {
     private Long id;
     private String title;
-    private String content;
+    private String name;
 }
