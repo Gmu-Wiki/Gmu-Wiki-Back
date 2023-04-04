c6a3b5851039c160c5e5de01aab795cbaad74003
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-04 12:50:07 +0900

    :memo: 현재 사용중인 비밀번호 검증식 변경 및 더티체킹 방식 사용

--- src/main/java/mpersand/Gmuwiki/domain/user/service/ChangePasswordService.java
+++ src/main/java/mpersand/Gmuwiki/domain/user/service/ChangePasswordService.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import mpersand.Gmuwiki.domain.auth.exception.MisMatchPasswordException;
 import mpersand.Gmuwiki.domain.user.entity.User;
 import mpersand.Gmuwiki.domain.user.presentation.dto.request.ChangePasswordRequest;
-import mpersand.Gmuwiki.domain.user.repository.UserRepository;
-import mpersand.Gmuwiki.global.util.EmailUtil;
 import mpersand.Gmuwiki.global.util.UserUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,17 +15,19 @@ public class ChangePasswordService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserUtil userUtil;
-    private final UserRepository userRepository;
 
     @Transactional(rollbackFor = Exception.class)
     public void execute(ChangePasswordRequest changePasswordRequest) {
 
         User user = userUtil.currentUser();
 
-        if (!passwordEncoder.matches(changePasswordRequest.getPassword(), user.getPassword())) {
+        if (passwordIsNotMatch(changePasswordRequest, user))
             throw new MisMatchPasswordException();
-        }
 
-        userRepository.save(user.updatePassword(passwordEncoder.encode(changePasswordRequest.getNewPassword())));
+        user.updatePassword(passwordEncoder.encode(changePasswordRequest.getNewPassword()));
+    }
+
+    private boolean passwordIsNotMatch(ChangePasswordRequest changePasswordRequest, User user) {
+        return !passwordEncoder.matches(changePasswordRequest.getPassword(), user.getPassword());
     }
 }
