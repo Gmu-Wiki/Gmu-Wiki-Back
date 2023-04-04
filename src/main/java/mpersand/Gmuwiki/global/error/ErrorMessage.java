1553987528b6b4a4032c255dc127b6858bf0823c
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-04 12:58:01 +0900

    :memo: @Builder 어노테이션 추가

--- src/main/java/mpersand/Gmuwiki/global/error/ErrorMessage.java
+++ src/main/java/mpersand/Gmuwiki/global/error/ErrorMessage.java
@@ -1,10 +1,12 @@
 package mpersand.Gmuwiki.global.error;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class ErrorMessage {
     private final String message;
     private final int status;
