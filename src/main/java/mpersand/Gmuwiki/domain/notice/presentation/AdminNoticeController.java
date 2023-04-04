8f65b6c04460f943de2faf1738a92a1e5625db36
jeong <jeongchangyo71@gmail.com>
2023-04-04 11:49:45 +0900

    :sparkles: CreateController 기능 생성

--- src/main/java/mpersand/Gmuwiki/domain/notice/presentation/AdminNoticeController.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/AdminNoticeController.java
@@ -1,14 +1,17 @@
 package mpersand.Gmuwiki.domain.notice.presentation;
 
 import lombok.RequiredArgsConstructor;
+import mpersand.Gmuwiki.domain.notice.presentation.dto.request.CreateNoticeRequest;
 import mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeDetailResponse;
 import mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeResponse;
+import mpersand.Gmuwiki.domain.notice.service.CreateNoticeService;
 import mpersand.Gmuwiki.domain.notice.service.ListNoticeService;
 import mpersand.Gmuwiki.domain.notice.service.OneNoticeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,6 +21,7 @@ public class AdminNoticeController {
 
     private final ListNoticeService listNoticeService;
     private final OneNoticeService oneNoticeService;
+    private final CreateNoticeService createNoticeService;
 
     @GetMapping
     public ResponseEntity<List<NoticeResponse>> findAll(){
@@ -29,5 +33,11 @@ public class AdminNoticeController {
     public ResponseEntity<NoticeDetailResponse> findOne(@PathVariable("id") Long id){
         NoticeDetailResponse oneFindById = oneNoticeService.execute(id);
         return new ResponseEntity<>(oneFindById,HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid CreateNoticeRequest createNoticeRequest){
+        createNoticeService.execute(createNoticeRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
