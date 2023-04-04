ec26fab5623083064c3e5d424edecb054737db7d
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-04 22:55:45 +0900

    :memo: 생성자 추가

--- src/main/java/mpersand/Gmuwiki/global/error/ErrorMessage.java
+++ src/main/java/mpersand/Gmuwiki/global/error/ErrorMessage.java
@@ -1,13 +1,15 @@
 package mpersand.Gmuwiki.global.error;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-@Builder
 public class ErrorMessage {
     private final String message;
     private final int status;
+
+    public ErrorMessage(ErrorCode errorCode){
+        this(errorCode.getMessage(), errorCode.getStatus());
+    }
 }
