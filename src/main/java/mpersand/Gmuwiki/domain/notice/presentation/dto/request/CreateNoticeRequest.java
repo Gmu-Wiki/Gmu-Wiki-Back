c087a269627168445d99d2f0299f76f6cf3d2c23
jeong <jeongchangyo71@gmail.com>
2023-04-04 11:50:08 +0900

    :memo: CreateNoticeRequest 작성

---
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/request/CreateNoticeRequest.java
@@ -0,0 +1,23 @@
+package mpersand.Gmuwiki.domain.notice.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateNoticeRequest {
+    @NotBlank(message = "제목은 필수 입력값입니다")
+    private String title;
+
+    @NotBlank(message = "내용은 필수 입력값입니다")
+    private String content;
+
+    @NotBlank(message = "이름은 필수입력값입니다")
+    private String name;
+}
