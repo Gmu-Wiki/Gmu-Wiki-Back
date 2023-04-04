96a37b43fc9f2128dea80d79fdaf47c7b0f762a5
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-04 22:57:03 +0900

    :memo: MethodArgumentNotValidException Handler 추가

--- src/main/java/mpersand/Gmuwiki/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/mpersand/Gmuwiki/global/error/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import mpersand.Gmuwiki.global.error.ErrorMessage;
 import mpersand.Gmuwiki.global.error.GimuwikiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,12 +17,20 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(GimuwikiException.class)
     public ResponseEntity<ErrorMessage> handleComprehensiveException(HttpServletRequest request, GimuwikiException e) {
-        printError(request, e, e.getErrorCode().getMessage());
+        printError(request, e.getErrorCode().getMessage());
         ErrorMessage errorMessage = new ErrorMessage(e.getErrorCode().getMessage(), e.getErrorCode().getStatus());
         return new ResponseEntity<>(errorMessage, HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
-    private void printError(HttpServletRequest request, GimuwikiException ex, String message) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> handleValidException(HttpServletRequest request, MethodArgumentNotValidException e) {
+        printError(request, "요청값이 유효하지 않습니다.");
+        ErrorMessage errorMessage = new ErrorMessage("요청값이 유효하지 않습니다.", HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+
+    private void printError(HttpServletRequest request, String message) {
         log.error(request.getRequestURI());
         log.error(message);
     }
