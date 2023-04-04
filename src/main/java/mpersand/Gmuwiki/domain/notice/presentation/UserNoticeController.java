5642e69629d4ba0e42e16eaa36b63c56e0871128
jeong <jeongchangyo71@gmail.com>
2023-04-04 11:53:56 +0900

    :poop: User는 Controller에 Create기능을 추가 해버림...

--- src/main/java/mpersand/Gmuwiki/domain/notice/presentation/UserNoticeController.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/UserNoticeController.java
@@ -1,13 +1,17 @@
 package mpersand.Gmuwiki.domain.notice.presentation;
 
 import lombok.RequiredArgsConstructor;
+import mpersand.Gmuwiki.domain.notice.presentation.dto.request.CreateNoticeRequest;
 import mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeDetailResponse;
 import mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeListResponse;
+import mpersand.Gmuwiki.domain.notice.service.CreateNoticeService;
 import mpersand.Gmuwiki.domain.notice.service.ListNoticeService;
 import mpersand.Gmuwiki.domain.notice.service.OneNoticeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/user/notice")
@@ -16,6 +20,7 @@ public class UserNoticeController {
 
     private final ListNoticeService listNoticeService;
     private final OneNoticeService oneNoticeService;
+    private final CreateNoticeService createNoticeService;
 
     @GetMapping
     public ResponseEntity<NoticeListResponse> findAll(){
@@ -28,4 +33,5 @@ public class UserNoticeController {
         NoticeDetailResponse oneFindById = oneNoticeService.execute(id);
         return new ResponseEntity<>(oneFindById,HttpStatus.OK);
     }
+
 }
