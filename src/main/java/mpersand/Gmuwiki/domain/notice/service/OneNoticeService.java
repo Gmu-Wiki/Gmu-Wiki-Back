ff4adb7bfcada72cbfe26c0943bb05f67dc1275e
jeong <jeongchangyo71@gmail.com>
2023-04-04 11:51:37 +0900

    :sparkles: User CreateController 생성

--- src/main/java/mpersand/Gmuwiki/domain/notice/service/OneNoticeService.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/service/OneNoticeService.java
@@ -2,12 +2,10 @@ package mpersand.Gmuwiki.domain.notice.service;
 
 import lombok.RequiredArgsConstructor;
 import mpersand.Gmuwiki.domain.notice.entity.Notice;
-import mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeIdResponse;
+import mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeDetailResponse;
 import mpersand.Gmuwiki.domain.notice.repository.NoticeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,10 +13,10 @@ public class OneNoticeService {
     private final NoticeRepository noticeRepository;
 
     @Transactional(readOnly = true)
-    public NoticeIdResponse execute(Long id) {
+    public NoticeDetailResponse execute(Long id) {
         Notice notice = noticeRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException());
-        NoticeIdResponse noticeIdResponse = NoticeIdResponse.builder()
+        NoticeDetailResponse noticeIdResponse = NoticeDetailResponse.builder()
                 .id(notice.getId())
                 .content(notice.getContent())
                 .title(notice.getTitle())
