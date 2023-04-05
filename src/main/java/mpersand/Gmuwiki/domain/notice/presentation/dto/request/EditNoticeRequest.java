30763ced6a45da9528429258e6788084efdf3747
jeong <jeongchangyo71@gmail.com>
2023-04-05 15:51:33 +0900

    :memo: notice 수정 request생성

---
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/request/EditNoticeRequest.java
@@ -0,0 +1,19 @@
+package mpersand.Gmuwiki.domain.notice.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EditNoticeRequest {
+
+    @NotBlank(message = "제목은 필수 입력값입니다")
+    private String title;
+
+    @NotBlank(message = "내용은 필수 입력값입니다")
+    private String content;
+}
