74ab39464a64dda7ec5fdb632d5c48b5e8d4797c
jeong <jeongchangyo71@gmail.com>
2023-04-10 23:13:48 +0900

    :ambulance: 공지 수정 시간 관련 오류 수정

--- src/main/java/mpersand/Gmuwiki/domain/notice/entity/Notice.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/entity/Notice.java
@@ -3,6 +3,10 @@ package mpersand.Gmuwiki.domain.notice.entity;
 import lombok.*;
 import mpersand.Gmuwiki.domain.notice.presentation.dto.request.EditNoticeRequest;
 import mpersand.Gmuwiki.domain.user.entity.User;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
@@ -10,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notice {
 
@@ -24,16 +29,17 @@ public class Notice {
     @Column(nullable = false)
     private String content;
 
-
     @Column(nullable = false)
     private String name;
 
     @ManyToOne
-    @JoinColumn(name = "id")
+    @JoinColumn(name = "user_id")
     private User user;
 
+    @CreatedDate
     private LocalDateTime createdDate;
 
+    @LastModifiedDate
     private LocalDateTime editedDate;
 
     public void update(EditNoticeRequest editNoticeRequest) {
