991fc3c7151c0393d53674238af904a708f9ddfc
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-13 20:10:20 +0900

    :memo: Board 추가

---
+++ src/main/java/mpersand/Gmuwiki/domain/board/entity/Board.java
@@ -0,0 +1,47 @@
+package mpersand.Gmuwiki.domain.board.entity;
+
+import lombok.*;
+import mpersand.Gmuwiki.domain.board.enums.BoardType;
+import mpersand.Gmuwiki.domain.user.entity.User;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private BoardType boardType;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime editedDate;
+}
