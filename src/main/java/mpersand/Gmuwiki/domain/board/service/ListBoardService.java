8d6c30a6ca9ab0e446e8da6670fa069d7833a96a
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-17 22:24:42 +0900

    :memo: @Transactional(readOnly = true) 제약 조건 추가, toResponse 메서드 변경 및 이동

--- src/main/java/mpersand/Gmuwiki/domain/board/service/ListBoardService.java
+++ src/main/java/mpersand/Gmuwiki/domain/board/service/ListBoardService.java
@@ -3,13 +3,15 @@ package mpersand.Gmuwiki.domain.board.service;
 import lombok.RequiredArgsConstructor;
 import mpersand.Gmuwiki.domain.board.entity.Board;
 import mpersand.Gmuwiki.domain.board.enums.BoardType;
-import mpersand.Gmuwiki.domain.board.presentation.dto.response.BoardResponse;
 import mpersand.Gmuwiki.domain.board.presentation.dto.response.ListBoardResponse;
 import mpersand.Gmuwiki.domain.board.repository.BoardRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static mpersand.Gmuwiki.domain.board.presentation.dto.response.BoardResponse.toResponse;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ public class ListBoardService {
 
     private final BoardRepository boardRepository;
 
+    @Transactional(readOnly = true)
     public ListBoardResponse execute(BoardType boardType) {
 
         List<Board> boards = boardRepository.findByBoardType(boardType);
@@ -30,12 +33,5 @@ public class ListBoardService {
                 .build();
 
         return listBoardResponse;
-    }
-
-    private BoardResponse toResponse(Board board) {
-
-        return BoardResponse.builder()
-                .title(board.getTitle())
-                .build();
     }
 }
