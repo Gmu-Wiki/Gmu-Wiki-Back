1b8873f60df07685a11144df67115836892b0e38
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-17 14:12:25 +0900

    :memo: BoardResponse 작성

---
+++ src/main/java/mpersand/Gmuwiki/domain/board/presentation/dto/response/BoardResponse.java
@@ -0,0 +1,13 @@
+package mpersand.Gmuwiki.domain.board.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class BoardResponse {
+
+    private String title;
+}
