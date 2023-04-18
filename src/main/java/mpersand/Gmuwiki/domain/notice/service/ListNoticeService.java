6472b672fef2488b488ac9430e13f12f615cbb5c
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-18 17:45:07 +0900

    :memo: toResponse 메서드 이동

--- src/main/java/mpersand/Gmuwiki/domain/notice/service/ListNoticeService.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/service/ListNoticeService.java
@@ -3,13 +3,14 @@ package mpersand.Gmuwiki.domain.notice.service;
 import lombok.RequiredArgsConstructor;
 import mpersand.Gmuwiki.domain.notice.entity.Notice;
 import mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeListResponse;
-import mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeResponse;
 import mpersand.Gmuwiki.domain.notice.repository.NoticeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static mpersand.Gmuwiki.domain.notice.presentation.dto.response.NoticeResponse.toResponse;
 
 
 @Service
@@ -19,7 +20,9 @@ public class ListNoticeService {
 
     @Transactional(readOnly = true)
     public NoticeListResponse execute() {
+
         List<Notice> notices = noticeRepository.findAll();
+
         NoticeListResponse noticeListResponse = NoticeListResponse.builder()
                 .list(
                         notices.stream()
@@ -27,14 +30,7 @@ public class ListNoticeService {
                                 .collect(Collectors.toList())
                 )
                 .build();
-        return noticeListResponse;
-    }
 
-    private NoticeResponse toResponse(Notice notice) {
-        return NoticeResponse.builder()
-                .id(notice.getId())
-                .title(notice.getTitle())
-                .name(notice.getName())
-                .build();
+        return noticeListResponse;
     }
 }
