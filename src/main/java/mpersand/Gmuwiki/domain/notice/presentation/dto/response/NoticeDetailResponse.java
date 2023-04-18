27a55c9d0d9d310c711cd59c8823abaa434fa819
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-18 17:56:28 +0900

    :memo: 공지 상세보기에 생성시간, 마지막 수정 시간 추가

--- src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/response/NoticeDetailResponse.java
+++ src/main/java/mpersand/Gmuwiki/domain/notice/presentation/dto/response/NoticeDetailResponse.java
@@ -2,15 +2,19 @@ package mpersand.Gmuwiki.domain.notice.presentation.dto.response;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NoticeDetailResponse {
 
-    private Long id;
     private String title;
-    private String name;
+
     private String content;
 
+    private LocalDateTime createdDate;
+
+    private LocalDateTime editedDate;
 }
