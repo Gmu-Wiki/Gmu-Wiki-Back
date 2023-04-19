857d7517433e0029e99582ac04df78017cc918bb
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-19 15:01:57 +0900

    :memo: 글 상세 보기에서 사용하는 DetailBoardResponse 객체의 이름을 알기 쉽게 변경

--- src/main/java/mpersand/Gmuwiki/domain/board/presentation/AdminBoardController.java
+++ src/main/java/mpersand/Gmuwiki/domain/board/presentation/AdminBoardController.java
@@ -39,7 +39,7 @@ public class AdminBoardController {
 
     @GetMapping("/{id}")
     public ResponseEntity<DetailBoardResponse> findOne(@PathVariable("id") Long id) {
-        DetailBoardResponse detailBoardResponse = oneBoardService.execute(id);
-        return new ResponseEntity<>(detailBoardResponse, HttpStatus.OK);
+        DetailBoardResponse oneFindById = oneBoardService.execute(id);
+        return new ResponseEntity<>(oneFindById, HttpStatus.OK);
     }
 }
