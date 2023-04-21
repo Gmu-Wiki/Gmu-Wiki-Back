6b494cb48de8ce9c7a44b29db2547d3ac6c7761f
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-21 11:58:30 +0900

    :memo: 서비스 클래스 이름 변경

--- src/main/java/mpersand/Gmuwiki/domain/board/service/DeleteBoardAdminService.java
+++ src/main/java/mpersand/Gmuwiki/domain/board/service/DeleteBoardAdminService.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class AdminDeleteBoardService {
+public class DeleteBoardAdminService {
 
     private final BoardRepository boardRepository;
 
