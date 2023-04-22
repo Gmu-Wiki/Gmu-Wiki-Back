c4061e84d312e5f0965b551ada26111c72a19b2f
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-22 22:35:23 +0900

    :memo: AnnotationExceptionService 생성

---
+++ src/main/java/mpersand/Gmuwiki/global/annotation/AnnotationExceptionService.java
@@ -0,0 +1,9 @@
+package mpersand.Gmuwiki.global.annotation;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public @interface AnnotationExceptionService {
+}
