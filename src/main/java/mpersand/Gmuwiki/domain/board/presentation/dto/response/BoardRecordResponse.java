e56acd5ef3b2bfd329542ebd501811e2eb3e47eb
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-27 18:03:49 +0900

    :memo: BoardRecordResponse 추가

---
+++ src/main/java/mpersand/Gmuwiki/domain/board/presentation/dto/response/BoardRecordResponse.java
@@ -0,0 +1,26 @@
+package mpersand.Gmuwiki.domain.board.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import mpersand.Gmuwiki.domain.board.entity.BoardRecord;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class BoardRecordResponse {
+
+    private LocalDateTime createdDate;
+
+    private String name;
+
+    public static BoardRecordResponse toResponse(BoardRecord boardRecord) {
+
+        return BoardRecordResponse.builder()
+                .createdDate(boardRecord.getCreatedDate())
+                .name(boardRecord.getName())
+                .build();
+    }
+}
