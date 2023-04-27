a1e43da9b758f479f85f00fda87d405841fc9e42
uuuuuuuk <s22034@gsm.hs.kr>
2023-04-27 18:14:35 +0900

    :memo: findByBoard 메서드 인터페이스 추가

--- src/main/java/mpersand/Gmuwiki/domain/board/repository/BoardRecordRepository.java
+++ src/main/java/mpersand/Gmuwiki/domain/board/repository/BoardRecordRepository.java
@@ -1,9 +1,15 @@
 package mpersand.Gmuwiki.domain.board.repository;
 
+import mpersand.Gmuwiki.domain.board.entity.Board;
 import mpersand.Gmuwiki.domain.board.entity.BoardRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRecordRepository extends JpaRepository<BoardRecord, Long> {
 
+    List<BoardRecord> findByBoard(Board board);
+
     boolean existsByTitle(String title);
+
 }
