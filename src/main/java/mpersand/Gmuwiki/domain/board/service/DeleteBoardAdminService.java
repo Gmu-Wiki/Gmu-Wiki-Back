b61adc6cd71f507779ff43a6cfce10d3aa9dfd37
uuuuuuuk <s22034@gsm.hs.kr>
2023-05-01 23:31:36 +0900

    :sparkles: DeleteBoardAdminService 에 원글을 지우기 전에 글 기록 테이블을 제거하는 코드 추가

--- src/main/java/mpersand/Gmuwiki/domain/board/service/DeleteBoardAdminService.java
+++ src/main/java/mpersand/Gmuwiki/domain/board/service/DeleteBoardAdminService.java
@@ -3,6 +3,7 @@ package mpersand.Gmuwiki.domain.board.service;
 import lombok.RequiredArgsConstructor;
 import mpersand.Gmuwiki.domain.board.entity.Board;
 import mpersand.Gmuwiki.domain.board.exception.BoardNotFoundException;
+import mpersand.Gmuwiki.domain.board.repository.BoardRecordRepository;
 import mpersand.Gmuwiki.domain.board.repository.BoardRepository;
 import mpersand.Gmuwiki.global.annotation.RollbackService;
 
@@ -12,10 +13,14 @@ public class DeleteBoardAdminService {
 
     private final BoardRepository boardRepository;
 
+    private final BoardRecordRepository boardRecordRepository;
+
     public void execute(Long id) {
 
         Board board = boardRepository.findById(id)
                 .orElseThrow(() -> new BoardNotFoundException());
+
+        boardRecordRepository.deleteAllByBoard(board);
 
         boardRepository.delete(board);
     }
