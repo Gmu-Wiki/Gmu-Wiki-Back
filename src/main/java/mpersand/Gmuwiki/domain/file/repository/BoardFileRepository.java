9d9c9dedc3f8b6f994f13c2c51b0bd0cbaa1e53a
uuuuuuuk <s22034@gsm.hs.kr>
2023-05-13 13:19:20 +0900

    :memo: BoardFileRepository 에 deleteAllByBoard, getBoardFileByBoardId 추상 메서드 추가

--- src/main/java/mpersand/Gmuwiki/domain/file/repository/BoardFileRepository.java
+++ src/main/java/mpersand/Gmuwiki/domain/file/repository/BoardFileRepository.java
@@ -1,7 +1,14 @@
 package mpersand.Gmuwiki.domain.file.repository;
 
+import mpersand.Gmuwiki.domain.board.entity.Board;
 import mpersand.Gmuwiki.domain.file.entity.BoardFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardFileRepository extends JpaRepository<BoardFile, Long> {
+
+    void deleteAllByBoard(Board board);
+
+    List<BoardFile> getBoardFileByBoardId(Long id);
 }
