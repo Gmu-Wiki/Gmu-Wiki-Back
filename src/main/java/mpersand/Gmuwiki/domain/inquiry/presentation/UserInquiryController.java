c243f492d570b897985598690ccfad425879f6a1
uuuuuuuk <s22034@gsm.hs.kr>
2023-05-18 20:06:38 +0900

    :sparkles: UserInquiryController 작성

--- src/main/java/mpersand/Gmuwiki/domain/inquiry/presentation/UserInquiryController.java
+++ src/main/java/mpersand/Gmuwiki/domain/inquiry/presentation/UserInquiryController.java
@@ -5,17 +5,14 @@ import mpersand.Gmuwiki.domain.inquiry.presentation.dto.request.InquirySendReque
 import mpersand.Gmuwiki.domain.inquiry.service.InquirySendService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/admin/inquiry")
-public class InquiryController {
+@RequestMapping("/user/inquiry")
+public class UserInquiryController {
 
     private final InquirySendService inquirySendService;
 
