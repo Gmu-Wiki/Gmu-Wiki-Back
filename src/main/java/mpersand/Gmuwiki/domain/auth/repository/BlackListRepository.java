ab87f0cbb13c6a2af38358d70c78fbfb9ae988f4
uuuuuuuk <s22034@gsm.hs.kr>
2023-06-07 08:44:21 +0900

    :fire: GAuth 로직에 따라 BlackListRepository 클래스 삭제

--- src/main/java/mpersand/Gmuwiki/domain/auth/repository/BlackListRepository.java
+++
@@ -1,7 +0,0 @@
-package mpersand.Gmuwiki.domain.auth.repository;
-
-import mpersand.Gmuwiki.domain.auth.entity.BlackList;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BlackListRepository extends CrudRepository<BlackList, String> {
-}
