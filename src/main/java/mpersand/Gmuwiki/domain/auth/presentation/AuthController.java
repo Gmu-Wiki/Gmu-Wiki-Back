faffd95563dfb581c89857395a68902a14c7a3f0
uuuuuuuk <s22034@gsm.hs.kr>
2023-06-07 08:38:42 +0900

    :sparkles: AuthController 를 GAuth 관련 로직으로 변경

--- src/main/java/mpersand/Gmuwiki/domain/auth/presentation/AuthController.java
+++ src/main/java/mpersand/Gmuwiki/domain/auth/presentation/AuthController.java
@@ -1,54 +1,59 @@
 package mpersand.Gmuwiki.domain.auth.presentation;
 
 import lombok.RequiredArgsConstructor;
-import mpersand.Gmuwiki.domain.auth.presentation.dto.request.LoginRequest;
-import mpersand.Gmuwiki.domain.auth.presentation.dto.request.SignUpRequest;
-import mpersand.Gmuwiki.domain.auth.presentation.dto.response.LoginResponse;
-import mpersand.Gmuwiki.domain.auth.presentation.dto.response.NewTokenResponse;
+import mpersand.Gmuwiki.domain.auth.presentation.data.dto.LoginDto;
+import mpersand.Gmuwiki.domain.auth.presentation.data.request.LoginRequest;
+import mpersand.Gmuwiki.domain.auth.presentation.data.response.LoginResponse;
+import mpersand.Gmuwiki.domain.auth.presentation.data.response.NewTokenResponse;
 import mpersand.Gmuwiki.domain.auth.sevice.TokenReissueService;
 import mpersand.Gmuwiki.domain.auth.sevice.UserLoginService;
 import mpersand.Gmuwiki.domain.auth.sevice.UserLogoutService;
-import mpersand.Gmuwiki.domain.auth.sevice.UserSignUpService;
+import mpersand.Gmuwiki.domain.auth.util.AuthConverter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final UserSignUpService userSignUpService;
     private final UserLoginService userLoginService;
 
     private final UserLogoutService userLogoutService;
+
     private final TokenReissueService tokenReissueService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
-        userSignUpService.execute(signUpRequest);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+    private final AuthConverter authConverter;
 
-    @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
-        LoginResponse loginResponse = userLoginService.execute(loginRequest);
+    @PostMapping
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) throws IOException {
+
+        LoginDto loginDto = authConverter.toDto(loginRequest);
+
+        LoginResponse loginResponse = userLoginService.execute(loginDto);
+
         return new ResponseEntity<>(loginResponse, HttpStatus.OK);
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> logout(@RequestHeader("Authorization")String accessToken){
-        userLogoutService.execute(accessToken);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-
-    }
-
     @PatchMapping
-    public ResponseEntity<NewTokenResponse> reIssueToken(@RequestHeader("RefreshToken") String token) {
-        NewTokenResponse reIssueToken = tokenReissueService.execute(token);
-        return new ResponseEntity<>(reIssueToken, HttpStatus.OK);
+    public ResponseEntity<NewTokenResponse> reIssueToken(@RequestHeader("Refresh-Token") String refreshToken) {
+
+        NewTokenResponse newTokenResponse = tokenReissueService.execute(refreshToken);
+
+        return new ResponseEntity<>(newTokenResponse, HttpStatus.OK);
     }
+
+    @DeleteMapping
+    public ResponseEntity<Void> logout(){
+
+        userLogoutService.execute();
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 
 }
