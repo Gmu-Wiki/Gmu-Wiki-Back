92506c17a25976eaad2e0708c47f754fe275b473
uuuuuuuk <s22034@gsm.hs.kr>
2023-06-07 08:46:30 +0900

    :fire: GAuth 로직에 따라 EmailAuthRepository 클래스 삭제

--- src/main/java/mpersand/Gmuwiki/domain/email/repository/EmailAuthRepository.java
+++
@@ -1,7 +0,0 @@
-package mpersand.Gmuwiki.domain.email.repository;
-
-import mpersand.Gmuwiki.domain.email.entity.EmailAuth;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmailAuthRepository extends CrudRepository<EmailAuth, String> {
-}
