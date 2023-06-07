d29b5a2cb4e4755750e2d17a5703a1bdff5be912
uuuuuuuk <s22034@gsm.hs.kr>
2023-06-07 09:07:58 +0900

    :memo: GAuth 로직에 따라 UserUtil 클래스 변경

--- src/main/java/mpersand/Gmuwiki/global/util/UserUtil.java
+++ src/main/java/mpersand/Gmuwiki/global/util/UserUtil.java
@@ -1,12 +1,10 @@
 package mpersand.Gmuwiki.global.util;
 
 import lombok.RequiredArgsConstructor;
-import mpersand.Gmuwiki.domain.auth.exception.MisMatchPasswordException;
 import mpersand.Gmuwiki.domain.user.exception.UserNotFoundException;
 import mpersand.Gmuwiki.domain.user.entity.User;
 import mpersand.Gmuwiki.domain.user.repository.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,21 +12,10 @@ import org.springframework.stereotype.Component;
 public class UserUtil {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
     public User currentUser() {
+
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UserNotFoundException());
-    }
-
-    public void checkPassword(User user, String password) {
-        if(!passwordEncoder.matches(password, user.getPassword())) {
-            throw new MisMatchPasswordException();
-        }
-    }
-
-    public User getUserByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UserNotFoundException());
     }
