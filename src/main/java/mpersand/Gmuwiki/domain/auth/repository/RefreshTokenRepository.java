78eac549f9ce2acd41c6718d278170ca442ebca8
uuuuuuuk <s22034@gsm.hs.kr>
2023-06-07 09:00:33 +0900

    :memo: RefreshTokenRepository 에 findByToken, findByUserId 추상 메서드 작성

--- src/main/java/mpersand/Gmuwiki/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/mpersand/Gmuwiki/domain/auth/repository/RefreshTokenRepository.java
@@ -3,5 +3,11 @@ package mpersand.Gmuwiki.domain.auth.repository;
 import mpersand.Gmuwiki.domain.auth.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+
+    RefreshToken findByToken(String token);
+
+    RefreshToken findByUserId(UUID userId);
 }
