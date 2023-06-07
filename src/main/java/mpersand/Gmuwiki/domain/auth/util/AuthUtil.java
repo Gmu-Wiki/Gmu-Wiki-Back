cf87d63923af2502a6e5f6691ca3e88adb545336
uuuuuuuk <s22034@gsm.hs.kr>
2023-06-07 09:08:34 +0900

    :memo: AuthUtil 작성

---
+++ src/main/java/mpersand/Gmuwiki/domain/auth/util/AuthUtil.java
@@ -0,0 +1,45 @@
+package mpersand.Gmuwiki.domain.auth.util;
+
+import gauth.GAuthUserInfo;
+import lombok.AllArgsConstructor;
+import mpersand.Gmuwiki.domain.auth.entity.RefreshToken;
+import mpersand.Gmuwiki.domain.auth.repository.RefreshTokenRepository;
+import mpersand.Gmuwiki.domain.user.entity.User;
+import mpersand.Gmuwiki.domain.user.repository.UserRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class AuthUtil {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    private final AuthConverter authConverter;
+
+    private final UserRepository userRepository;
+
+    public void saveNewUser(GAuthUserInfo gAuthUserInfo, String refreshToken) {
+
+        User signInUserInfo = authConverter.toEntity(gAuthUserInfo);
+
+        userRepository.save(signInUserInfo);
+
+        saveNewRefreshToken(signInUserInfo, refreshToken);
+    }
+
+    public void saveNewAdmin(GAuthUserInfo gAuthUserInfo, String refreshToken) {
+
+        User signInAdminInfo = authConverter.toAdminEntity(gAuthUserInfo);
+
+        userRepository.save(signInAdminInfo);
+
+        saveNewRefreshToken(signInAdminInfo, refreshToken);
+    }
+
+    public RefreshToken saveNewRefreshToken(User userInfo, String refreshToken) {
+
+        RefreshToken newRefreshToken = authConverter.toEntity(userInfo, refreshToken);
+
+        return refreshTokenRepository.save(newRefreshToken);
+    }
+}
