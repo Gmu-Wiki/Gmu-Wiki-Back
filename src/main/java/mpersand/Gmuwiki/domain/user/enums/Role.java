680284fd6c6832b74dc7688b32c42ce0e8a170ec
uuuuuuuk <s22034@gsm.hs.kr>
2023-06-07 09:01:26 +0900

    :memo: Role enum 에 상수 이름 변경

--- src/main/java/mpersand/Gmuwiki/domain/user/enums/Role.java
+++ src/main/java/mpersand/Gmuwiki/domain/user/enums/Role.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER, ADMIN;
+
+    ROLE_STUDENT, ROLE_ADMIN;
+
     @JsonCreator
     public static Role from(String s) {
         return Role.valueOf(s.toUpperCase());
