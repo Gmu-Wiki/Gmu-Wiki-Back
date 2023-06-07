d9f437abb81c7df5b3c26abf6adc35e1bbc8693c
uuuuuuuk <s22034@gsm.hs.kr>
2023-06-07 10:38:25 +0900

    :memo: GrantAdminRequest 작성

---
+++ src/main/java/mpersand/Gmuwiki/domain/user/presentation/dto/request/GrantAdminRequest.java
@@ -0,0 +1,14 @@
+package mpersand.Gmuwiki.domain.user.presentation.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GrantAdminRequest {
+
+    private String email;
+}
