856e8eb78e60a1f95354e9b7b293afbe752c76a9
uuuuuuuk <s22034@gsm.hs.kr>
2023-06-21 23:32:24 +0900

    :memo: 문의사항 거부 메일 디자인 변경

--- src/main/java/mpersand/Gmuwiki/domain/inquiry/service/InquiryRefusalService.java
+++ src/main/java/mpersand/Gmuwiki/domain/inquiry/service/InquiryRefusalService.java
@@ -40,22 +40,47 @@ public class InquiryRefusalService {
 
         String subject = "G무위키 문의 답변이 도착했습니다!";
 
-        String content="";
-        content+= "<div style='margin:100px;'>";
-        content+= "<h1> 안녕하세요 G무위키입니다! </h1>";
-        content+= "<br>";
-        content+= "<h2><p>G무위키를 이용해주셔서 감사합니다. 귀하가 신청하신<p></h2>" + inquiryName + "<h2><p>에 대한 문의가 거부되었습니다.";
-        content+= "<br>";
-        content+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        content+= "<h3 style='color:blue;'>거부된 사유는 다음과 같습니다.</h3>";
-        content+= "<div style='font-size:100%'>";
-        content+= "거부 사유 : <strong>";
-        content+= comment +"</strong><div><br/> ";
-        content+= "</div>";
-        content+= "</div>";
-        content+= "</div>";
-        content+= "<br>";
-        content+= "<p>추가로 궁금한 사항이 있다면 디스코드 최민욱#9522으로 보내주시면 최대한 빠르게 확인하겠습니다. 감사합니다.<p>";
+        String content="<html lang=\"en\">\n" +
+                "  <head>\n" +
+                "    <meta charset=\"UTF-8\" />\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n" +
+                "    <title>Document</title>\n" +
+                "  </head>\n" +
+                "  <body>\n" +
+                "    <div style='margin: 100px'>\n" +
+                "      <h1>\n" +
+                "        안녕하세요!\n" +
+                "        <p style='color: #007eff; margin: 0; padding: 0; display: inline'>\n" +
+                "          G무위키\n" +
+                "        </p>\n" +
+                "        입니다!\n" +
+                "      </h1>\n" +
+                "      <br />\n" +
+                "      <h2>G무위키를 이용해주셔서 감사합니다. 귀하가 신청하신</h2> " + inquiryName + " <h2>에 대한 문의가 거부되었습니다.</h2>\n" +
+                "      <br />\n" +
+                "      <div\n" +
+                "        align='center'\n" +
+                "        style='\n" +
+                "          border: 0px solid black;\n" +
+                "          font-family: verdana;\n" +
+                "          border: 1px solid #000;\n" +
+                "          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);\n" +
+                "          border-radius: 5px;\n" +
+                "        '\n" +
+                "      >\n" +
+                "        <h3 style='color: #007eff'>거부된 사유는 다음과 같습니다.</h3>\n" +
+                "        <div style='font-size: 100%'>\n" +
+                "            거부 사유 : <strong> " + comment + " </strong>\n" +
+                "          <div><br /></div>\n" +
+                "        </div>\n" +
+                "      </div>\n" +
+                "      <br />\n" +
+                "        추가로 궁금한 사항이 있다면 디스코드 최민욱#9522으로 보내주시면 최대한\n" +
+                "        빠르게 확인하겠습니다. 감사합니다.\n" +
+                "      \n" +
+                "  </body>\n" +
+                "</html>";
+
 
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
