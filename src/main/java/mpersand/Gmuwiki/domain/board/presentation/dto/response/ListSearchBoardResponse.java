5009f72c782d5a7083b68585f5b0aeb3244b1db3
uuuuuuuk <s22034@gsm.hs.kr>
2023-07-11 23:27:09 +0900

    :memo: ListSearchBoardResponse 작성

---
+++ src/main/java/mpersand/Gmuwiki/domain/board/presentation/dto/response/ListSearchBoardResponse.java
@@ -0,0 +1,15 @@
+package mpersand.Gmuwiki.domain.board.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ListSearchBoardResponse {
+
+    List<SearchBoardResponse> boardTitleList;
+}
