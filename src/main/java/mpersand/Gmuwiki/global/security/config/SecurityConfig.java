ba54b149513732eaf3ffb7463c4ff531e3e9da87
uuuuuuuk <s22034@gsm.hs.kr>
2023-07-29 22:25:54 +0900

    :fire: 필요없는 코드 삭제

--- src/main/java/mpersand/Gmuwiki/global/security/config/SecurityConfig.java
+++ src/main/java/mpersand/Gmuwiki/global/security/config/SecurityConfig.java
@@ -6,13 +6,9 @@ import mpersand.Gmuwiki.global.filter.JwtRequestFilter;
 import mpersand.Gmuwiki.global.logger.filter.LogRequestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsUtils;
@@ -51,15 +47,5 @@ public class SecurityConfig {
                 .addFilterBefore(logRequestFilter, ExceptionFilter.class);
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
     }
 }
